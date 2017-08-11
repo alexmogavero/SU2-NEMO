e61fdb35a44c3aacf33deb70f2e47e3c81808e1f
Alessandro Mogavero <alessandro.mogavero@strath.ac.uk>
2017-08-11 12:05:16 +0200

    Added class CElectronicExcitation the implementation of the new gas model is not complete

---
+++ SU2_CFD/include/CElectronicExcitation.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "CVibrationArmonics.hpp"
+
+/*!
+ * \class CElectronicExcitation
+ * \brief Class that model a Maxwell-Boltzmann gas with electronic excitation model for specific heat.
+ * \author: A. Mogavero
+ * \version 5.0.0 "Raven"
+ */
+class CElectronicExcitation : public CVibrationArmonics {
+	std::vector<su2double> g;
+	std::vector<su2double> ThetaEl;
+
+	su2double Cv(su2double T)const;
+
+	/*! \brief Class to perform boost integration. */
+	class Int_Cv {
+
+	    const CElectronicExcitation* obj;
+
+	public:
+	    Int_Cv( const CElectronicExcitation* f );
+
+	    void operator() ( const su2double &x , su2double &dxdt , const double T );
+	};
+
+protected:
+
+  virtual su2double Energy(su2double T)const;
+  virtual su2double SpecificHeatVol(su2double T)const;
+  virtual su2double EntropyTemp(su2double T)const;
+
+public:
+
+     /*!
+     * \brief Constructor of the class.
+     */
+    CElectronicExcitation(void);
+
+    /*!
+     * \brief Constructor of the class.
+     */
+    CElectronicExcitation(su2double R, su2double g, su2double theta,
+    		std::vector<su2double> gEl, std::vector<su2double> thetaEl);
+
+
+    /*!
+     * \brief Destructor of the class.
+     */
+    virtual ~CElectronicExcitation(void);
+
+
+};
