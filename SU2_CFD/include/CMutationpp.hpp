94b406824d9da145506c13a3a6f0a5e392435b24
Alessandro Mogavero <alessandro.mogavero@strath.ac.uk>
2017-10-13 16:33:05 +0100

    Added new class CMutationpp

---
+++ SU2_CFD/include/CMutationpp.hpp
@@ -0,0 +1,109 @@
+#pragma once
+
+#include "fluid_model.hpp"
+#include "mutation++.h"
+
+/*!
+ * \class CMutationpp
+ * \brief Wrapper gas to use Mutation++ gas model.
+ * \author: A. Mogavero
+ * \version 5.0.0 "Raven"
+ */
+class CMutationpp : public CFluidModel {
+
+  const su2double Gas_Constant; //!<Ideal gas constant of the mixture
+
+  su2double CalcGasConstant()const; //!Calculates the ideal gas constant of the mixture.
+
+  Mutation::MixtureOptions opt; //!<option object used to create mix
+  Mutation::Mixture mix; //!<Mixture object
+
+  vector<double> comp; //!<Mixture composition in molar fraction
+
+  /*!
+   * \brief Calculate the partial density of every specie
+   * @param rho total density of mixture
+   * @return a density for each specie
+   */
+  vector<double> SpecieDensity(su2double rho)const;
+
+  /*!
+   * \brief Set the state so that it is equal to the underlying mutation++ state
+   */
+  void UpdateState();
+
+public:
+
+     /*!
+     * \brief Constructor of the class.
+     */
+    CMutationpp(void);
+
+    /*!
+     * \brief Constructor of the class.
+     */
+    CMutationpp(string R, vector<double>);
+
+
+    /*!
+     * \brief Destructor of the class.
+     */
+    virtual ~CMutationpp(void);
+
+    /*!
+     * \brief Set the Dimensionless State using Density and Internal Energy
+     * \param[in] rho - first thermodynamic variable.
+     * \param[in] e - second thermodynamic variable.
+     */
+
+    void SetTDState_rhoe (su2double rho, su2double e );
+
+    /*!
+     * \brief Set the Dimensionless State using Pressure  and Temperature
+     * \param[in] P - first thermodynamic variable.
+     * \param[in] T - second thermodynamic variable.
+     */
+
+    void SetTDState_PT (su2double P, su2double T );
+
+    /*!
+     * \brief Set the Dimensionless State using Pressure and Density
+     * \param[in] P - first thermodynamic variable.
+     * \param[in] rho - second thermodynamic variable.
+     */
+
+    void SetTDState_Prho (su2double P, su2double rho );
+
+    /*!
+     * \brief Set the Dimensionless Internal Energy using Pressure and Density
+     * \param[in] P - first thermodynamic variable.
+     * \param[in] rho - second thermodynamic variable.
+     */
+
+    void SetEnergy_Prho (su2double P, su2double rho );
+
+    /*!
+     * \brief Set the Dimensionless State using Enthalpy and Entropy
+     * \param[in] th1 - first thermodynamic variable (h).
+     * \param[in] th2 - second thermodynamic variable (s).
+     *
+     */
+    void SetTDState_hs (su2double h, su2double s );
+
+
+    /*!
+     * \brief Set the Dimensionless State using Density and Temperature
+     * \param[in] th1 - first thermodynamic variable (rho).
+     * \param[in] th2 - second thermodynamic variable (T).
+     *
+     */
+    void SetTDState_rhoT (su2double rho, su2double T );
+
+    /*!
+     * \brief Set the Dimensionless State using Pressure and Entropy
+     * \param[in] th1 - first thermodynamic variable (P).
+     * \param[in] th2 - second thermodynamic variable (s).
+     */
+
+    void SetTDState_Ps (su2double P, su2double s );
+};
---
+++ SU2_CFD/src/CMutationpp.cpp
@@ -0,0 +1,136 @@
+/*!
+ * CMutationpp.cpp
+ * \brief Source of mutation++ wrapper.
+ * \author A. Mogavero
+ * \version 5.0.0 "Raven"
+ *
+ * SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
+ *                      Dr. Thomas D. Economon (economon@stanford.edu).
+ *
+ * SU2 Developers: Prof. Juan J. Alonso's group at Stanford University.
+ *                 Prof. Piero Colonna's group at Delft University of Technology.
+ *                 Prof. Nicolas R. Gauger's group at Kaiserslautern University of Technology.
+ *                 Prof. Alberto Guardone's group at Polytechnic University of Milan.
+ *                 Prof. Rafael Palacios' group at Imperial College London.
+ *                 Prof. Edwin van der Weide's group at the University of Twente.
+ *                 Prof. Vincent Terrapon's group at the University of Liege.
+ *
+ * Copyright (C) 2012-2017 SU2, the open-source CFD code.
+ *
+ * SU2 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SU2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../include/CMutationpp.hpp"
+#include <functional>
+#include <boost/math/tools/roots.hpp>
+
+CMutationpp::CMutationpp() :
+	CFluidModel(),
+	opt("air5"),
+	mix(opt),
+	comp(),
+	Gas_Constant(0){
+}
+
+
+CMutationpp::CMutationpp(string optFile, vector<double> cmp):
+	CFluidModel(),
+	opt(optFile),
+	mix(opt),
+	comp(cmp),
+	Gas_Constant(CalcGasConstant()){
+}
+
+su2double CMutationpp::CalcGasConstant()const{
+  su2double R = 0;
+  for(size_t i=0; i<comp.size(); i++){
+    R += comp[i]*mix.speciesMw(i);
+  }
+
+  return R;
+}
+
+
+CMutationpp::~CMutationpp(void) {
+
+}
+
+void CMutationpp::SetTDState_rhoe (su2double rho, su2double e ) {
+  vector<double> rhoSpecie = SpecieDensity(rho);
+  mix.setState(rhoSpecie.data(), &e, 0);
+
+  UpdateState();
+}
+
+vector<double> CMutationpp::SpecieDensity(su2double rho)const{
+  vector<double> rhoSpecie(comp.size(), 0);
+  mix.convert<Mutation::Thermodynamics::CONC_TO_RHO>(comp.data(), rhoSpecie.data());
+  double rhoFact = 0;
+  for(size_t i=0; i<comp.size(); i++){
+    rhoFact += rhoSpecie[i];
+  }
+  rhoFact = rho/rhoFact;
+  for(size_t i=0; i<comp.size(); i++){
+    rhoSpecie[i] *= rhoFact;
+  }
+
+  return rhoSpecie;
+}
+
+void CMutationpp::UpdateState(){
+  Density = mix.density();
+  StaticEnergy = mix.mixtureEnergyMass();
+  Temperature = mix.T();
+  Pressure = mix.P();
+
+  Cp = mix.mixtureFrozenCpMass();
+
+  SoundSpeed2 = pow(mix.frozenSoundSpeed(), 2);
+
+  Entropy = mix.mixtureSMass();
+
+  dPdrho_e = Pressure/Density;
+  dTdrho_e = 0.0;
+  dTde_rho = 1/mix.mixtureFrozenCvMass();
+  dPde_rho = Pressure/Temperature*dTde_rho;
+}
+
+void CMutationpp::SetTDState_PT (su2double P, su2double T){
+  su2double rho = P/(T*Gas_Constant);
+  SetTDState_rhoT(rho, T);
+}
+
+void CMutationpp::SetTDState_Prho (su2double P, su2double rho ) {
+  su2double T = P/(Gas_Constant*rho);
+  SetTDState_rhoT(rho, T);
+
+}
+
+void CMutationpp::SetTDState_hs (su2double h, su2double s ) {
+  throw std::logic_error("Error: method SetTDState_hs not implemented in class CMutationpp.");
+}
+
+void CMutationpp::SetTDState_Ps (su2double P, su2double s ) {
+	throw std::logic_error("Error: method SetTDState_Ps not implemented in class CMutationpp.");
+}
+
+void CMutationpp::SetTDState_rhoT (su2double rho, su2double T ) {
+  vector<double> rhoSpecie = SpecieDensity(rho);
+  mix.setState(rhoSpecie.data(), &T, 1);
+
+  UpdateState();
+}
+
+
+
